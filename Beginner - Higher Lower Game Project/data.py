84458917a723c3a8022174f44ea8247f67ffc9a5
ADEL_AIT_MOUALI <48539633+ADELAITMOUALI@users.noreply.github.com>
2024-10-05 23:43:09 +0100

    Add files via upload

---
+++ Beginner - Higher Lower Game Project/data.py
@@ -0,0 +1,77 @@
+data = [
+    {
+        "name": "Lionel Messi",
+        "followers": 480000000,
+        "job": "football"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "followers": 600000000,
+        "job": "football"
+    },
+    {
+        "name": "Taylor Swift",
+        "followers": 340000000,
+        "job": "musical"
+    },
+    {
+        "name": "Kylian Mbappé",
+        "followers": 100000000,
+        "job": "football"
+    },
+    {
+        "name": "Drake",
+        "followers": 250000000,
+        "job": "musical"
+    },
+    {
+        "name": "Neymar Jr.",
+        "followers": 220000000,
+        "job": "football"
+    },
+    {
+        "name": "Ariana Grande",
+        "followers": 300000000,
+        "job": "musical"
+    },
+    {
+        "name": "Mohamed Salah",
+        "followers": 58000000,
+        "job": "football"
+    },
+    {
+        "name": "Rihanna",
+        "followers": 240000000,
+        "job": "musical"
+    },
+    {
+        "name": "Kevin De Bruyne",
+        "followers": 45000000,
+        "job": "football"
+    },
+    {
+        "name": "Ed Sheeran",
+        "followers": 210000000,
+        "job": "musical"
+    },
+    {
+        "name": "Karim Benzema",
+        "followers": 47000000,
+        "job": "football"
+    },
+    {
+        "name": "Beyoncé",
+        "followers": 280000000,
+        "job": "musical"
+    },
+    {
+        "name": "Sadio Mané",
+        "followers": 18000000,
+        "job": "football"
+    },
+    {
+        "name": "Billie Eilish",
+        "followers": 120000000,
+        "job": "musical"
+    }
+]
